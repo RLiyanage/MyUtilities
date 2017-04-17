7ac27af49eb0d24da22c3a1d017260677eb89c93
Sanuth <Sanuth@192.168.2.12>
2017-04-17 11:11:52 -0500

    Added Date Utilities class.

---
+++ src/edu/wctc/rnn/date/DateUtilities.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.rnn.date;
+
+/**
+ * Represents local date time for setting date or time.
+ * @author Rasika Nanayakkara,rliyanage@my.wctc.edu
+ * @since 1.8;
+ * @version 1.00;
+ */
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import static java.time.temporal.ChronoUnit.DAYS;
+import java.time.temporal.TemporalAdjusters;
+import static java.time.temporal.TemporalQueries.localDate;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Sanuth
+ */
+public class DateUtilities {
+/**
+ * Gets the future date
+ * @return future date from now 
+ */
+    public LocalDate getFutureDate() {
+        int days = 15;
+        LocalDate today = LocalDate.now();
+        LocalDate futureDay = today.plus(days, ChronoUnit.DAYS);
+        System.out.println(futureDay);
+        return futureDay;
+    }
+/** 
+ *@see LocalDateTime To FormattedString.
+ 
+ */    
+
+    public void convertToLocalDateTimeToFormattedString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.of(0, Month.of(0), 0, 0, 0);
+        String formattedDateTime = dateTime.format(formatter);
+        System.out.println(formattedDateTime);
+    }
+    /** 
+*@see  LocalDateTime To FormattedString.
+@param dateAndTime - date and time in String format.
+ */ 
+
+    public void convertFornmattedStringToLocalDateTime(String dateAndTime) {
+        dateAndTime = "2016-04-08 12:30";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(dateAndTime, formatter);
+        System.out.println(dateTime);
+    }
+/**
+ * @see two days difference in years, months and days.
+ * @param startDate - start date is required to calculate duration.
+ * @param endDate - end date is required to calculate duration.
+ */
+    public static void calculateDuration() {
+        LocalDate startDate = LocalDate.of(2015, 4, 9);
+        LocalDate endDate = LocalDate.of(2017, 5, 9);
+        Period duration = Period.between(startDate, endDate);
+        System.out.println((duration.getYears())+(duration.getMonths())+(duration.getDays()));
+        
+        
+    }
+}
